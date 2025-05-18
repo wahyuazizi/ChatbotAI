4d9709ece79abd976d6d0cfc6cda37702cae7ec4
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-05-18 20:33:37 +0800

    setup user input

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
+import { MessageWithButton } from './MessageFrom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,18 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MessageWithButton />
     </>
   )
 }
--- frontend/src/MessageFrom.jsx
+++ frontend/src/MessageFrom.jsx
@@ -1,26 +1,27 @@
-import * as React from "react"
-
+import { useState } from 'react'
 import { Button } from "@/components/ui/button"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
+import axios from 'axios'
 
-export function MessageCardForm(){
-    
+const api = axios.create({
+    baseURL: "http://localhost:5173/"
+})
+
+export function MessageWithButton() {
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Button clicked");
+        const response = await api.post("/chat", {message: question})
+        setAnswer(response.data.answer)
+    }
+
+    return (
+        <div className="grid items-center justify-center gap-2">
+            <Textarea value={question} onChange={(e) => setQuestion(e.target.value)} placeholder="Type your message here." />
+            <Button type='submit' onClick={handleSubmit} >Kirim</Button>
+        </div>
+    )
 }
